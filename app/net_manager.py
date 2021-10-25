e918caddde9b2363b36b272ec263ac138f286cbb
Lucas Demeneck <luc-dem@hotmail.com>
2021-10-25 16:16:54 -0300

    Adding items to net_list_widget

--- app/net_manager.py
+++ app/net_manager.py
@@ -1,21 +1,41 @@
-from typing import List
+from typing import List, Callable, Dict
 
-from neural_network import NeuralNetwork, LinearNeuron
+from neural_network import LinearNeuron
+from .extended_neural_network import ExtendedNeuralNetwork
 
 
 class NetManager():
 	def __init__(self):
-		self.net_by_id = {}
+		self.net_by_id: Dict[int, ExtendedNeuralNetwork] = {}
 		self.selected_net_id = -1
 		self.last_assigned_id = -1
-		pass
+		self.selection_change_listeners: List[Callable] = []
+		# self.update_listeners: List[Callable] = []
+		# self.net_removal_listeners: List[Callable] = []
 
-	def new_net(self) -> int:
-		net = NeuralNetwork(LinearNeuron, 0, [0])
+	@property
+	def selected_net(self) -> ExtendedNeuralNetwork:
+		return self.net_by_id[self.selected_net_id]
+
+	def new_net(self):
+		net = ExtendedNeuralNetwork("New net", LinearNeuron, 0, [0])
 		self.last_assigned_id += 1
 		id = self.last_assigned_id
 		self.net_by_id[id] = net
-		return id
+		self.change_selected_net(id)
 
 	def remove_net(self, id):
-		self.neural_nets.pop(id)
+		self.net_by_id.pop(id)
+		# for listener in self.net_removal_listeners:
+		# 	listener()
+
+	def change_selected_net(self, selected_id):
+		if selected_id == self.selected_net_id:
+			return
+		self.selected_net_id = selected_id
+		for listener in self.selection_change_listeners:
+			listener()
+
+	# def update_net(self):
+	# 	for listener in self.update_listeners:
+	# 		listener()
