a10a70b0fb0986c0eb1b505b1c960f7d2cdd9c80
Lucas Demeneck <luc-dem@hotmail.com>
2021-10-30 21:05:27 -0300

    file selection

--- gui/load_file_widget.py
+++ gui/load_file_widget.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QGroupBox, QHBoxLayout, QLabel, QLineEdit, QPushButton, QVBoxLayout, QScrollArea, QWidget
+from PyQt5.QtWidgets import QFileDialog, QGroupBox, QHBoxLayout, QLabel, QLineEdit, QPushButton, QWidget
 
 
 class LoadFileWidget(QWidget):
@@ -6,6 +6,14 @@ class LoadFileWidget(QWidget):
 		super().__init__()
 		self.setLayout(QHBoxLayout())
 		self.layout().addWidget(QLabel("File Path:"))
-		self.layout().addWidget(QLineEdit())
-		self.file_dialog_button = QPushButton()
+		self.file_path_input = QLineEdit()
+		self.layout().addWidget(self.file_path_input)
+		self.file_dialog_button = QPushButton("Select\nfile")
+		self.file_dialog_button.clicked.connect(self.file_dialog)
+		self.file_dialog_button.setFixedWidth(self.file_dialog_button.fontMetrics().width(self.file_dialog_button.text()))
+		self.file_dialog_button.setFixedHeight(self.file_dialog_button.width())
 		self.layout().addWidget(self.file_dialog_button)
+
+	def file_dialog(self):
+		self.file_path_input.setText(QFileDialog.getOpenFileName(self, filter="CSV (*.csv);;JSON (*.json)")[0])
+		pass
