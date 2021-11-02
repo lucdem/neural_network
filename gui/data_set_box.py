06f772175f6fcfa89c397337955f7dfec9b0eba9
Lucas Demeneck <luc-dem@hotmail.com>
2021-11-02 20:35:45 -0300

    removing unescessary widgets

--- gui/data_set_box.py
+++ gui/data_set_box.py
@@ -1,6 +1,4 @@
-from PyQt5.QtWidgets import QGroupBox, QVBoxLayout
-
-from .load_file_widget import LoadFileWidget
+from PyQt5.QtWidgets import QGroupBox, QHBoxLayout, QSizePolicy, QLineEdit, QPushButton, QLabel, QFileDialog
 
 
 class DataSetBox(QGroupBox):
@@ -8,5 +6,19 @@ class DataSetBox(QGroupBox):
 		super().__init__()
 
 		self.setTitle(title)
-		self.setLayout(QVBoxLayout())
-		self.layout().addWidget(LoadFileWidget())
+
+		self.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Maximum)
+
+		self.setLayout(QHBoxLayout())
+		self.layout().addWidget(QLabel("File Path:"))
+		self.file_path_input = QLineEdit()
+		self.layout().addWidget(self.file_path_input)
+		self.file_dialog_button = QPushButton("Select\nfile")
+		self.file_dialog_button.clicked.connect(self.file_dialog)
+		self.file_dialog_button.setFixedWidth(self.file_dialog_button.fontMetrics().width(self.file_dialog_button.text()))
+		self.file_dialog_button.setFixedHeight(self.file_dialog_button.width())
+		self.layout().addWidget(self.file_dialog_button)
+
+	def file_dialog(self):
+		self.file_path_input.setText(QFileDialog.getOpenFileName(self, filter="CSV (*.csv);;JSON (*.json)")[0])
+		pass
--- gui/main_window.py
+++ gui/main_window.py
@@ -8,7 +8,6 @@ from .net_params_box import NetParamsBox
 from .training_params_box import TrainingParamsBox
 from .layer_params_box import LayerParamsBox
 from .net_list_widget import NetListWidget
-from gui import net_list_widget
 
 
 class MainWindow(QWidget):
@@ -21,37 +20,32 @@ class MainWindow(QWidget):
 		# build window
 		self.top_layer_layout = QHBoxLayout()
 		self.net_list_widget = NetListWidget(net_manager)
-		self.center_widget = QWidget()
-		self.center_widget.setLayout(QVBoxLayout())
+		self.center_layout = QVBoxLayout()
 		self.graph_widget = self.graph_wrapper.widget
 
 		self.setWindowTitle("Neural Network")
 		self.setLayout(self.top_layer_layout)
 
 		self.top_layer_layout.addWidget(self.net_list_widget, stretch=1)
-		self.top_layer_layout.addWidget(self.center_widget, stretch=3)
+		self.top_layer_layout.addLayout(self.center_layout, stretch=3)
 		self.top_layer_layout.addWidget(self.graph_widget, stretch=3)
 
 		self.net_params_box = NetParamsBox(net_manager)
-		self.center_widget.layout().addWidget(self.net_params_box, stretch=1)
+		self.center_layout.addWidget(self.net_params_box, stretch=1)
 
 		self.layer_params_box = LayerParamsBox(net_manager)
-		self.center_widget.layout().addWidget(self.layer_params_box, stretch=1)
+		self.center_layout.addWidget(self.layer_params_box, stretch=1)
 
 		self.training_params_box = TrainingParamsBox(net_manager)
-		self.center_widget.layout().addWidget(self.training_params_box, stretch=1)
+		self.center_layout.addWidget(self.training_params_box, stretch=1)
 
-		data_sets_widget = QWidget()
-		data_sets_widget.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Maximum)
-		data_sets_widget.setLayout(QHBoxLayout())
-		data_sets_widget.layout().setSpacing(5)
-		data_sets_widget.layout().setContentsMargins(0, 0, 0, 0)
-		data_sets_widget.layout().addWidget(DataSetBox("Training Set"))
-		data_sets_widget.layout().addWidget(DataSetBox("Validation Set"))
-		self.center_widget.layout().addWidget(data_sets_widget, stretch=1)
+		data_sets_layout = QHBoxLayout()
+		data_sets_layout.addWidget(DataSetBox("Training Set"))
+		data_sets_layout.addWidget(DataSetBox("Validation Set"))
+		self.center_layout.addLayout(data_sets_layout, stretch=1)
 
 		self.text_output = TextOutputWidget()
-		self.center_widget.layout().addWidget(self.text_output, stretch=0)
+		self.center_layout.addWidget(self.text_output, stretch=0)
 
 		# event/signal bindings
 
