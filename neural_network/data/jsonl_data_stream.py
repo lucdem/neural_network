483556b2087cfc893776ad41a20e42be84f744a2
Lucas Demeneck <luc-dem@hotmail.com>
2021-11-13 10:43:57 -0300

    added validation

--- neural_network/data/jsonl_data_stream.py
+++ neural_network/data/jsonl_data_stream.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import jsonlines
-from typing import Iterable
+from typing import Iterable, List
 
 from .data import Data, DataSample
 from .data_point import DataPoint
@@ -8,23 +8,28 @@ from .data_point import DataPoint
 
 class JsonlDataStream(Data):
 	def __init__(self, jsonl_file_path):
-		self.file = open(jsonl_file_path)
-		self.reader = jsonlines.Reader(self.file)
+		self.file_path = jsonl_file_path
+
+	def get_data_points(self) -> List[DataPoint]:
+		with jsonlines.Reader(open(self.file_path)) as reader:
+			data_points = []
+			while True:
+				try:
+					data_points.append(DataPoint.fromDict(reader.read()))
+				except EOFError:
+					break
+		return data_points
 
 	def split_batches(self, batch_size: int) -> Iterable[DataSample]:
-		""" Randomly distributes data points in batches.
-			Last batch can have size smaller than batch_size to contain
-			leftover data points
-		"""
-
-		batch = []
-		while True:
-			try:
-				batch.append(DataPoint.fromDict(self.reader.read()))
-			except EOFError:
-				break
-			if len(batch) == batch_size:
+		with jsonlines.Reader(open(self.file_path)) as reader:
+			batch = []
+			while True:
+				try:
+					batch.append(DataPoint.fromDict(reader.read()))
+				except EOFError:
+					break
+				if len(batch) == batch_size:
+					yield DataSample(batch)
+					batch = []
+			if len(batch) > 0:
 				yield DataSample(batch)
-				batch = []
-		if len(batch) > 0:
-			yield DataSample(batch)
