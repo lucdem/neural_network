3fe8644d0fea1e1c401bb48797ff25acd6804a6c
Lucas Demeneck <luc-dem@hotmail.com>
2021-11-24 15:14:20 -0300

    Implemented saving network as json file

--- gui/net_list_widget.py
+++ gui/net_list_widget.py
@@ -1,7 +1,11 @@
-from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QMenu
+from __future__ import annotations
+import os
+import json
+
+from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QMenu, QFileDialog
 from PyQt5.QtCore import pyqtSignal
 
-from app import NetManager
+from app import NetManager, NetJsonEnconder
 
 
 class NetListWidget(QListWidget):
@@ -26,6 +30,9 @@ class NetListWidget(QListWidget):
 			remove_net_action = menu.addAction('Remove net')
 			remove_net_action.triggered.connect(lambda: self.remove_net(item))
 
+			save_net_action = menu.addAction("Save net")
+			save_net_action.triggered.connect(lambda: self.save_net(item))
+
 		menu.exec(event.globalPos())
 
 	def new_net(self):
@@ -37,6 +44,17 @@ class NetListWidget(QListWidget):
 	def remove_net(self, item):
 		self.net_manager.remove_net(item.net_id)
 		self.removeItemWidget(item)
+
+	def save_net(self, item: NetListItem):
+		default_filter = "JSON (.json)"
+		path = QFileDialog.getSaveFileName(self, filter = default_filter, initialFilter = default_filter)[0]
+		if path is None:
+			return
+		split_path = os.path.splitext(path)
+		if split_path[1] != ".json":
+			path = "".join((split_path[0], ".json"))
+		with open(path, mode='w') as f:
+			f.write(json.dumps(self.net_manager.net_by_id[item.net_id], cls = NetJsonEnconder))
 
 	def change_selected_net_name(self, name):
 		self.currentItem().setText(name)
