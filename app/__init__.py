c9ac3e9b2596e2a75424f4d56a7dc14ddc17dcc5
Lucas Demeneck <luc-dem@hotmail.com>
2021-11-24 15:14:20 -0300

    Implemented saving network as json file

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,4 @@
 from .net_manager import NetManager
 from .neuron_type_enum import NeuronTypeEnum
+from .extended_neural_network import ExtendedNeuralNetwork, NetJsonDecoder, NetJsonEnconder
+from .cost_function_enum import CostFunctionEnum
--- app/extended_neural_network.py
+++ app/extended_neural_network.py
@@ -1,9 +1,69 @@
-from typing import Type, List
+from typing import Dict, Type, List
+from json import JSONEncoder, JSONDecoder
+
+import numpy
 
 from neural_network import NeuralNetwork, Neuron
+from neural_network.layer import Layer
+
+from .neuron_type_enum import NeuronTypeEnum
 
 
 class ExtendedNeuralNetwork(NeuralNetwork):
 	def __init__(self, name, neuron_type: Type[Neuron], input_count, layer_sizes: List[int]):
 		super().__init__(neuron_type, input_count, layer_sizes)
 		self.name = name
+
+	def __getstate__(self):
+		return {
+			'name': self.name,
+			'input_count': self.input_count,
+			'layers': [{
+				'neuron_type': NeuronTypeEnum(type(layer.neurons[0])).name,
+				'weight_matrix': [neuron.weights for neuron in layer.neurons]
+			} for layer in self.layers]
+		}
+
+	def __setstate__(self, state):
+		layers = []
+		for layer_state in state['layers']:
+			neurons = []
+			for weights in range(layer_state['weight_matrix'].shape[1]):
+				neuron = NeuronTypeEnum[layer_state['neuron_type']].value(weights.size)
+				neuron.weights = weights
+			layers.append(Layer.create_from_neurons(neurons))
+
+		self.layers = layers
+
+
+class NetJsonEnconder(JSONEncoder):
+	def default(self, net: ExtendedNeuralNetwork):
+		if isinstance(net, ExtendedNeuralNetwork):
+			return {
+				'name': net.name,
+				'input_count': net.input_count,
+				'layers': [{
+					'neuron_type': NeuronTypeEnum(type(layer.neurons[0])).name,
+					'weight_matrix': [neuron.weights.tolist() for neuron in layer.neurons],
+					'biases': [neuron.bias for neuron in layer.neurons]
+				} for layer in net.layers]
+			}
+		return super().default()
+
+
+class NetJsonDecoder(JSONDecoder):
+	def __init__(self) -> None:
+		super().__init__(object_hook = self._net_decode)
+
+	def _net_decode(self, dict: Dict):
+		weights = dict.get('weight_matrix')
+		if weights is None:
+			return ExtendedNeuralNetwork.create_from_layers(dict['layers'])
+		else:
+			neurons = []
+			for i, weights in enumerate(dict['weight_matrix']):
+				neuron = NeuronTypeEnum[dict['neuron_type']].value(len(weights))
+				neuron.weights = numpy.array(weights)
+				neuron.bias = dict['biases'][i]
+				neurons.append(neuron)
+			return Layer.create_from_neurons(neurons)
