a90a80156185f1c4cd680c7765a01d49e760e0bf
Lucas Demeneck <luc-dem@hotmail.com>
2021-12-14 12:43:17 -0300

    Added momentum gradient descent

--- app/data_classes/training_params.py
+++ app/data_classes/training_params.py
@@ -1,8 +1,9 @@
 class TrainingParams:
 	def __init__(self, training_data_path: str, validation_data_path: str,
-			learning_rate: float, batch_size: int, max_epochs: int):
+			learning_rate: float, friction: float, batch_size: int, max_epochs: int):
 		self.training_data_path = training_data_path
 		self.validation_data_path = validation_data_path
 		self.learning_rate = learning_rate
+		self.friction = friction
 		self.batch_size = batch_size
 		self.max_epochs = max_epochs
